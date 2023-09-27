76c043210b6d839f566330fa5f86b7298a5da8cd
zhengxinzhen <zhengxinzhen@taqu.cn>
2023-09-27 15:03:10 +0800

    预加载脚本_隔离暴露函数

--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,6 @@
-const { ipcRenderer, remote } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
+
+// 别去研究 remote 相关的了，以后要废弃了
 
 console.log('预加载脚本执行')
 
@@ -13,3 +15,14 @@ ipcRenderer.on('renderer_event_listen_1', (event, args) => {
 
 // 给主进程发送消息
 ipcRenderer.send('asynchronous-message', {'msg': "msg_for_main_process_xixi-haha_1"})
+
+// reloadjs中的方法和变量都是与页面中的js隔离的，要暴露方法给页面使用，可以使用 contextBridge 来传递
+// - window中要 new BrowserWindow() 中 webPreferences.contextIsolation 要为true（默认为true），才会有效果
+contextBridge.exposeInMainWorld( // 在页面中就可以使用 window.electron.doThing(); 来调用
+    'electron',
+    {
+        doThing: () => {
+            console.log('exposeInMainWorld_info')
+        }
+    }
+);
